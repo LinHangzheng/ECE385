3451efc18649448a57562f92335e8aabd6e06b0a
Lin <hangzheng.17@intl.zju.edu.cn>
2020-04-15 22:21:36 +0800

    add soc file

---
+++ lab7/lab7_soc/lab7_soc_bb.v
@@ -0,0 +1,34 @@
+
+module lab7_soc (
+	clk_clk,
+	key_export_export,
+	led_wire_export,
+	reset_reset_n,
+	sdram_clk_clk,
+	sdram_wire_addr,
+	sdram_wire_ba,
+	sdram_wire_cas_n,
+	sdram_wire_cke,
+	sdram_wire_cs_n,
+	sdram_wire_dq,
+	sdram_wire_dqm,
+	sdram_wire_ras_n,
+	sdram_wire_we_n,
+	sw_export_export);	
+
+	input		clk_clk;
+	input	[3:0]	key_export_export;
+	output	[7:0]	led_wire_export;
+	input		reset_reset_n;
+	output		sdram_clk_clk;
+	output	[12:0]	sdram_wire_addr;
+	output	[1:0]	sdram_wire_ba;
+	output		sdram_wire_cas_n;
+	output		sdram_wire_cke;
+	output	[1:0]	sdram_wire_cs_n;
+	inout	[15:0]	sdram_wire_dq;
+	output	[1:0]	sdram_wire_dqm;
+	output		sdram_wire_ras_n;
+	output		sdram_wire_we_n;
+	input	[7:0]	sw_export_export;
+endmodule
---
+++ lab7/lab7_soc/lab7_soc_inst.vhd
@@ -0,0 +1,39 @@
+	component lab7_soc is
+		port (
+			clk_clk           : in    std_logic                     := 'X';             -- clk
+			key_export_export : in    std_logic_vector(3 downto 0)  := (others => 'X'); -- export
+			led_wire_export   : out   std_logic_vector(7 downto 0);                     -- export
+			reset_reset_n     : in    std_logic                     := 'X';             -- reset_n
+			sdram_clk_clk     : out   std_logic;                                        -- clk
+			sdram_wire_addr   : out   std_logic_vector(12 downto 0);                    -- addr
+			sdram_wire_ba     : out   std_logic_vector(1 downto 0);                     -- ba
+			sdram_wire_cas_n  : out   std_logic;                                        -- cas_n
+			sdram_wire_cke    : out   std_logic;                                        -- cke
+			sdram_wire_cs_n   : out   std_logic_vector(1 downto 0);                     -- cs_n
+			sdram_wire_dq     : inout std_logic_vector(15 downto 0) := (others => 'X'); -- dq
+			sdram_wire_dqm    : out   std_logic_vector(1 downto 0);                     -- dqm
+			sdram_wire_ras_n  : out   std_logic;                                        -- ras_n
+			sdram_wire_we_n   : out   std_logic;                                        -- we_n
+			sw_export_export  : in    std_logic_vector(7 downto 0)  := (others => 'X')  -- export
+		);
+	end component lab7_soc;
+
+	u0 : component lab7_soc
+		port map (
+			clk_clk           => CONNECTED_TO_clk_clk,           --        clk.clk
+			key_export_export => CONNECTED_TO_key_export_export, -- key_export.export
+			led_wire_export   => CONNECTED_TO_led_wire_export,   --   led_wire.export
+			reset_reset_n     => CONNECTED_TO_reset_reset_n,     --      reset.reset_n
+			sdram_clk_clk     => CONNECTED_TO_sdram_clk_clk,     --  sdram_clk.clk
+			sdram_wire_addr   => CONNECTED_TO_sdram_wire_addr,   -- sdram_wire.addr
+			sdram_wire_ba     => CONNECTED_TO_sdram_wire_ba,     --           .ba
+			sdram_wire_cas_n  => CONNECTED_TO_sdram_wire_cas_n,  --           .cas_n
+			sdram_wire_cke    => CONNECTED_TO_sdram_wire_cke,    --           .cke
+			sdram_wire_cs_n   => CONNECTED_TO_sdram_wire_cs_n,   --           .cs_n
+			sdram_wire_dq     => CONNECTED_TO_sdram_wire_dq,     --           .dq
+			sdram_wire_dqm    => CONNECTED_TO_sdram_wire_dqm,    --           .dqm
+			sdram_wire_ras_n  => CONNECTED_TO_sdram_wire_ras_n,  --           .ras_n
+			sdram_wire_we_n   => CONNECTED_TO_sdram_wire_we_n,   --           .we_n
+			sw_export_export  => CONNECTED_TO_sw_export_export   --  sw_export.export
+		);
+
